607c08005d5dc4f75920fa6f81a8108f54148e41
toumash <mr.toumash@gmail.com>
2018-08-17 17:55:30 +0200

    adds basic setup for api project

---
+++ wcindicator.api/wcindicator.api/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wcindicator.api.Models;
+
+namespace wcindicator.api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vm = new IndexPageViewModel();
+            vm.StatusString = "API W.I.P State";
+            return View(vm);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
