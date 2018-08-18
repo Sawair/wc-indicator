ad3d0bb0447d7947809921a49daee59698b0a1d4
toumash <mr.toumash@gmail.com>
2018-08-18 10:44:35 +0200

    adds tests for home page and api to be responsive

---
+++ wcindicator.api/wcindicator.api.test/ApiTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace wcindicator.api.test
+{
+    public class ApiTest
+        : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public ApiTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/home/index")]
+        public async Task HomePage_ShouldBeAccessibleOnTwoAddresses(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/api/status")]
+        [InlineData("/api/report")]
+        public async Task StatusApiShouldReturnApplicationJson(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
