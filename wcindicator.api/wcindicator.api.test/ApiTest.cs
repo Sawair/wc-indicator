cbb30c9536273d8b5f4b47b751c25e4a18e4e363
toumash <mr.toumash@gmail.com>
2018-08-18 11:15:56 +0200

    adds api tests

--- wcindicator.api/wcindicator.api.test/ApiTest.cs
+++ wcindicator.api/wcindicator.api.test/ApiTest.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using wcindicator.api.Controllers;
+using wcindicator.api.Models;
+using wcindicator.api.test.Extensions;
 using Xunit;
 
 namespace wcindicator.api.test
@@ -49,6 +54,36 @@ namespace wcindicator.api.test
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
+        }
+
+        [Fact]
+        public async Task StatusApiShouldReturnApplicationJsonType()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/report");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Func<Task> a = () => response.Content.ReadAsJsonStrictAsync<StatusReport>();
+            a.Should().NotThrow<JsonSerializationException>();
+        }
+
+        [Fact]
+        public async Task StatusApiShouldReturnApplicationJsonType2()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/report");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsJsonStrictAsync<StatusReport>();
+            json.Id.Should().BeGreaterOrEqualTo(1);
         }
     }
 }
