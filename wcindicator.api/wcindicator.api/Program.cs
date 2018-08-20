5b3222599de025bdfdff16282e96e998e676d8bd
toumash <mr.toumash@gmail.com>
2018-08-20 14:35:11 +0200

    adds iis integration

--- wcindicator.api/wcindicator.api/Program.cs
+++ wcindicator.api/wcindicator.api/Program.cs
@@ -12,6 +12,7 @@ namespace wcindicator.api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseIISIntegration();
     }
 }
