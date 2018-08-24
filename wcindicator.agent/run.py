f86c88486207a48cd28a2c0c0c6cb59590c6742a
Sawair <czerkies2805@gmail.com>
2018-08-24 17:54:45 +0200

    Adds heartbeat

--- wcindicator.agent/run.py
+++ wcindicator.agent/run.py
@@ -1,14 +1,19 @@
 from pyA20.gpio import port
 from pyA20.gpio import gpio
 
+from threading import Thread
 from datetime import datetime
 from time import sleep
 
 import json
+import time
 
 # config
 inputPort = port.PA11
-reportServerAddress = 'http://chcesiku.pl/api/status'
+reportServerAddress = 'http://chcesiku.pl'
+statusEndPoint = '/api/status'
+heartbeatEndPoint = '/api/heartbeat'
+interval = 10
 # end config
 
 import urllib2
@@ -51,12 +56,22 @@ class WCStateManager:
         print('status= %s.data=%s' % (response.code, d))
 
 
+def startHeartbeat(interval, reportServer):
+    while True:
+        response = http_post(reportServer,{},'')
+        print('Heartbeat status= %s' % response.code)
+        time.sleep(interval)
+
+
 gpio.init()
 gpio.setcfg(inputPort, gpio.INPUT)
 gpio.pullup(inputPort, 0)
 gpio.pullup(inputPort, gpio.PULLDOWN)
 
-manager = WCStateManager(reportServerAddress)
+manager = WCStateManager(reportServerAddress+statusEndPoint)
+
+t = Thread(target=startHeartbeat, args=(interval, reportServerAddress+heartbeatEndPoint))
+t.start()
 
 while True:
     if gpio.input(inputPort) == 1:
